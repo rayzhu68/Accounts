71d34d1f80f18b0907c3846dc2f0475454566906
Ray Zhu <jz6868@me.com>
2018-11-28 13:35:57 +0000

    Messages to tell logout so that logout functionality is done

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, reverse
-from django.contrib import auth
+from django.contrib import auth, messages
 
 def index(request):
     return render(request, 'index.html')
 
 def logout(request):
     auth.logout(request)
+    messages.success(request, "You have successfuly been logout!")
     return redirect(reverse('index'))
