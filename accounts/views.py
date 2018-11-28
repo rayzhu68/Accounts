3041fc132199ac65ad98c1bbee7e38ceb4ec00bc
Ray Zhu <jz6868@me.com>
2018-11-28 16:52:48 +0000

    wiring login functionality

--- accounts/views.py
+++ accounts/views.py
@@ -7,11 +7,24 @@ def index(request):
 
 def logout(request):
     auth.logout(request)
-    messages.success(request, "You have successfuly been logout!")
+    messages.success(request, "You have successfuly been logged out!")
     return redirect(reverse('index'))
     
 def login(request):
-    login_form = UserLoginForm()
+    if request.method=="POST":
+        login_form = UserLoginForm(request.POST)
+        
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password'])
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+            else:
+                login_form.add_error(None, "Your username or password is incorrect!")
+    else:
+        login_form = UserLoginForm()
+    
     return render(request, 'login.html', {"login_form": login_form})
     
     
