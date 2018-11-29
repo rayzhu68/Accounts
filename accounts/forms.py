9a6906578e80314f691670f223a93cf4c0ac85de
Ray Zhu <jz6868@me.com>
2018-11-29 14:15:02 +0000

    user registration form by UserCreationForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,23 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 class UserLoginForm(forms.Form):
     """Form to be used to log users in"""
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(UserCreationForm):
+    """Form to be used to register a new user"""
+    
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password Confirmation",
+        widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password1', 'password2']
