699407fbc54d5bdc795c850dc9c3aea1bef4e1dd
Ray Zhu <jz6868@me.com>
2018-11-30 08:37:35 +0000

    user registration form validation, clean_, form logic and view logic

--- accounts/views.py
+++ accounts/views.py
@@ -23,6 +23,7 @@ def login(request):
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect!")
     else:
@@ -32,6 +33,26 @@ def login(request):
     
 def registration(request):
     """Render the registration page"""
-    
-    registration_form=UserRegistrationForm()
-    return render(request, 'registration.html', {"registration_form": registration_form})
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+        
+    if request.method == "POST":
+        registration_form = UserRegistrationForm(request.POST)
+        
+        if registration_form.is_valid():
+            registration_form.save()
+            
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password1'])
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully registered")
+                return redirect(reverse('index'))
+            else:
+                messages.error(request, "unable to register your account at this time")
+    else:
+         registration_form = UserRegistrationForm()
+            
+    return render(request, 
+        'registration.html', 
+        {"registration_form": registration_form})
