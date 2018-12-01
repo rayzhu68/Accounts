8af4f92b30750ca6a67a9b0008a7816b0ea15a71
Ray Zhu <jz6868@me.com>
2018-12-01 16:06:14 +0000

    email to user and reset password

--- jango_auth/settings.py
+++ jango_auth/settings.py
@@ -122,4 +122,13 @@ STATIC_URL = '/static/'
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+#EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'jz6868@gmail.com'
+EMAIL_HOST_PASSWORD = 'ShaobinWu1963'
+EMAIL_PORT = 587
+
+#EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+#EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
